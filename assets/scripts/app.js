d419e857fdd0aefb89f349308b1e3eff72a20b5a
Kane <Pperson599@gmail.com>
2020-03-12 11:32:35 -0400

    Game logic complete
    -finished game with win and Draw
    -added new game button
    -hide game board from start

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,66 +2,73 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const store = require('./store')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
-  const gameState = ['', '', '', '', '', '', '', '', '']
-  let turn = 'x'
-  let gameOver = false
-  const winCondition = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [0, 4, 8], [2, 5, 8], [0, 4, 7], [2, 4, 6]]
+  let gameCount = 0
+  let turn = 'X'
+
+  const changeTurn = function () {
+    if (turn === 'X') {
+      turn = 'O'
+    } else {
+      turn = 'X'
+    }
+  }
+
+  const winner = function () {
+    store.gameOver = true
+    $('#Game').text(turn + ' is the winner')
+    gameCount += 1
+  }
+
+  const checkWin = function () {
+    for (let i = 0; i < 8; i++) {
+      if (store.winCondition[i].every(v => store.gameState[v] === 'X') || store.winCondition[i].every(v => store.gameState[v] === 'O')) {
+        winner()
+      }
+    }
+  }
+  const checkDraw = function () {
+    if ((store.gameState.every(v => v !== '')) && (store.gameOver === false)) {
+      $('#Game').text('Draw!')
+      store.gameOver = true
+    }
+  }
+
+  const newGame = function (event) {
+    event.preventDefault()
+    $('.container').removeClass('hidden')
+    $('.box').text('')
+    $('#Game').text('')
+    $('#message').text('')
+    store.gameState = ['', '', '', '', '', '', '', '', '']
+    store.gameOver = false
+  }
 
   const fillSpace = function (event) {
-    console.log('working')
     event.preventDefault()
     // get the position in the aray that they moved to
     const position = event.target.id
-    console.log('clicked ' + position)
     // get the curent text of the space the y chose
     const space = $(event.target).text()
-    console.log(space)
     // if space is open
-    if (space !== 'x' && space !== 'o' && gameOver === false) {
-      console.log('valid space ' + '(' + space + ')')
+    if (space !== 'X' && space !== 'O' && store.gameOver === false) {
       // add them to the boad
       $(event.target).text(turn)
-      gameState.splice(position, 1, turn)
+      store.gameState.splice(position, 1, turn)
+      console.log(store.gameState)
       // check for winner
-      if ((gameState[0] === 'x' && gameState[1] === 'x' && gameState[2] === 'x') ||
-(gameState[3] === 'x' && gameState[4] === 'x' && gameState[5] === 'x') ||
-(gameState[6] === 'x' && gameState[7] === 'x' && gameState[8] === 'x') ||
-(gameState[0] === 'x' && gameState[3] === 'x' && gameState[6] === 'x') ||
-(gameState[0] === 'x' && gameState[4] === 'x' && gameState[8] === 'x') ||
-(gameState[2] === 'x' && gameState[5] === 'x' && gameState[8] === 'x') ||
-(gameState[0] === 'x' && gameState[4] === 'x' && gameState[7] === 'x') ||
-(gameState[2] === 'x' && gameState[4] === 'x' && gameState[6] === 'x')) {
-        console.log('game over')
-        gameOver = true
-        $('#Game').text('X is the Winner!')
-      } else if
-      ((gameState[0] === 'o' && gameState[1] === 'o' && gameState[2] === 'o') ||
-(gameState[3] === 'o' && gameState[4] === 'o' && gameState[5] === 'o') ||
-(gameState[6] === 'o' && gameState[7] === 'o' && gameState[8] === 'o') ||
-(gameState[0] === 'o' && gameState[3] === 'o' && gameState[6] === 'o') ||
-(gameState[0] === 'o' && gameState[4] === 'o' && gameState[8] === 'o') ||
-(gameState[2] === 'o' && gameState[5] === 'o' && gameState[8] === 'o') ||
-(gameState[0] === 'o' && gameState[4] === 'o' && gameState[7] === 'o') ||
-(gameState[2] === 'o' && gameState[4] === 'o' && gameState[6] === 'o')) {
-        console.log('game over')
-        gameOver = true
-        $('#Game').text('O is the Winner!')
-      }
-      // winStates.forEach()
-      // change the turn
-      console.log(gameState)
-      if (turn === 'x') {
-        turn = 'o'
+      if (checkWin() === true) {
+        winner()
       } else {
-        turn = 'x'
+        checkDraw()
+        changeTurn()
       }
-    } else if (gameOver === true) {
+    } else if (store.gameOver === true) {
       $('#message').text('Game is over')
     } else {
       $('#message').text('invalid move')
@@ -69,4 +76,8 @@ $(() => {
   }
 
   $('.box').on('click', fillSpace)
+  $('#new-game').on('submit', newGame)
+  $('game-count').on('submit', function (){
+
+  })
 })
--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,6 +1,21 @@
 'use strict'
+const winCondition = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [0, 4, 8],
+  [2, 5, 8],
+  [0, 4, 7],
+  [2, 4, 6]]
 
-const store = {
+const gameState = ['', '', '', '', '', '', '', '', '']
+
+const gameOver = false
+
+module.exports = {
+  winCondition,
+  gameState,
+  gameOver
+
 }
-
-module.exports = store
